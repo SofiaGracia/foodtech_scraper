59c6c6709a8e81ac85c84f20fa5840468eb1a2d9
sofia <sofia@neverlate.es>
2025-09-15 11:45:27 +0200

    Basic initial scraping code. Added initial docker-compose.yml

--- scraper/main.py
+++ scraper/main.py
@@ -3,6 +3,10 @@ from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 import time
+import csv
+import os
+
+os.makedirs("data", exist_ok=True)
 
 # Install and set the driver automatically
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
@@ -11,7 +15,7 @@ driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 driver.get("https://es.openfoodfacts.org/")
 
 # Wait 5 seconds
-time.sleep(4)
+time.sleep(2)
 
 # Product info
 name_products = []
@@ -27,11 +31,22 @@ for p_c in product_contents:
     name_products.append(p_c.find_element(By.CLASS_NAME,"list_product_name").text)
 
     quality_infos = p_c.find_elements(By.CLASS_NAME,"list_product_icons")
+
+    # Manage the possible lack of info
+
     
-    nu_score = (quality_infos[0].get_attribute("title"))
-    n_score = quality_infos[1].get_attribute("title")
-    g_score = quality_infos[2].get_attribute("title")
+    
+    nutri_score.append(quality_infos[0].get_attribute("title"))
+    nova_score.append(quality_infos[1].get_attribute("title"))
+    green_score.append(quality_infos[2].get_attribute("title"))
 
 
 # Close the browser
 driver.quit()
+
+data = zip(name_products, nutri_score, nova_score, green_score)
+
+with open('data/first_fifty.csv', 'w', newline='', encoding='utf-8') as f:
+    writer = csv.writer(f)
+    writer.writerow(["Nombre del producto", "Nutri-Score", "Nova-Score", "Green-Score"])
+    writer.writerows(data)
