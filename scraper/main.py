3cd898b7734b9695c507c3f3fc3ea8f245bd6817
sofia <sofiagracia000@gmail.com>
2025-09-15 12:53:27 +0200

    Managing possible errors

--- scraper/main.py
+++ scraper/main.py
@@ -2,7 +2,8 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
-import time
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import csv
 import os
 
@@ -15,16 +16,18 @@ driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 # Open the page
 driver.get("https://es.openfoodfacts.org/")
 
-# Wait 5 seconds
-time.sleep(2)
-
 # Product info
 name_products = []
 nutri_score = []
 nova_score = []
 green_score = []
 
-product_contents = driver.find_elements(By.CLASS_NAME,"list_product_content")
+info = [nutri_score, nova_score, green_score]
+
+# Wait for the elements to appear before scraping
+# Create a WebDriverWait object
+wait = WebDriverWait(driver=driver, timeout=10)
+product_contents = wait.until(EC.visibility_of_all_elements_located((By.CLASS_NAME,"list_product_content")))
 
 for p_c in product_contents:
 
@@ -34,13 +37,13 @@ for p_c in product_contents:
     quality_infos = p_c.find_elements(By.CLASS_NAME,"list_product_icons")
 
     # Manage the possible lack of info
-
-
-    
-    nutri_score.append(quality_infos[0].get_attribute("title"))
-    nova_score.append(quality_infos[1].get_attribute("title"))
-    green_score.append(quality_infos[2].get_attribute("title"))
-
+    for i in range(3):
+        if i < len(quality_infos):
+            quality_info = quality_infos[i].get_attribute("title")
+            info[i].append(quality_info if quality_info is not None else "N/A")
+        else:
+            # In case there's not enough icons
+            info[i].append("N/A")
 
 # Close the browser
 driver.quit()
